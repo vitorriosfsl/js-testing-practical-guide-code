3d85b953305f3ced35cc3f380f6d63dd9cd0a416
Vitor Rios <vitor.rios@fullstacklabs.co>
2022-11-17 11:35:09 -0300

    test: updating  hook tests

--- code/05-hooks-starting-code/hooks/hooks.test.js
+++ code/05-hooks-starting-code/hooks/hooks.test.js
@@ -1,9 +1,9 @@
 import { it, expect } from 'vitest';
 
 import { User } from './hooks';
+const testEmail = 'test@test.com';
 
 it('should update the email', () => {
-  const testEmail = 'test@test.com';
   const newTestEmail = 'test2@test.com';
 
   const user = new User(testEmail);
@@ -13,24 +13,18 @@ it('should update the email', () => {
 });
 
 it('should have an email property', () => {
-  const testEmail = 'test@test.com';
-
   const user = new User(testEmail);
 
   expect(user).toHaveProperty('email');
 });
 
 it('should store the provided email value', () => {
-  const testEmail = 'test@test.com';
-
   const user = new User(testEmail);
 
   expect(user.email).toBe(testEmail);
 });
 
 it('should clear the email', () => {
-  const testEmail = 'test@test.com';
-
   const user = new User(testEmail);
   user.clearEmail();
 
@@ -38,8 +32,6 @@ it('should clear the email', () => {
 });
 
 it('should still have an email property after clearing the email', () => {
-  const testEmail = 'test@test.com';
-
   const user = new User(testEmail);
   user.clearEmail();
 
